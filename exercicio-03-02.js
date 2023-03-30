da428ffb7542fdfc9d53dc60bb928ae6dd75ad32
João Cláudio <joaoclaudiobio@gmail.com>
2023-03-30 12:18:24 -0300

    exercicio 4

--- exercicio-03-02.js
+++ exercicio-03-02.js
@@ -23,8 +23,25 @@
 
 // ############ EXERCICIO 3 #################
 
+// for (let i =0; i< numbers.length; i +=1){
+//     soma += numbers [i];
+// }
+
+// console.log (soma/numbers.length);
+
+// ########## EXERCICIO 4 ###########
+
 for (let i =0; i< numbers.length; i +=1){
     soma += numbers [i];
 }
 
-console.log (soma/numbers.length);
+let resultado = soma/numbers.length;
+
+
+if (resultado > 20){
+    console.log (resultado + " Então o valor é maior que 20");
+}
+else {
+    console.log ( resultado + " Então o valor é menor ou igual a 20");
+}
+
