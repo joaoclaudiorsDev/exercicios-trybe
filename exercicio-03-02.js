07de19542e12977c6302aa2073f77909fbd50a9e
João Cláudio <joaoclaudiobio@gmail.com>
2023-03-30 12:00:32 -0300

    exercicio2

--- exercicio-03-02.js
+++ exercicio-03-02.js
@@ -1,4 +1,6 @@
  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+ let soma = 0;
+// ############# EXERCICIO 1 ###################
 
 // for (let i = 0; i < numbers.length; i +=1){
 //     console.log (numbers[i]);
@@ -12,3 +14,9 @@
 //     console.log (numbers[i]);
 // }
 
+// ############### EXERCICIO 2 ############################
+
+for (let i = 0; i < numbers.length; i += 1){
+    soma += numbers [i];
+}
+console.log (soma);
