ebcbb62619c6137502e307d56cae244e7d64ece9
João Cláudio <joaoclaudiobio@gmail.com>
2023-03-30 12:39:18 -0300

    exercicio 5

--- exercicio-03-02.js
+++ exercicio-03-02.js
@@ -1,5 +1,6 @@
  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
  let soma = 0;
+ let numeroMaior = 0;
 // ############# EXERCICIO 1 ###################
 
 // for (let i = 0; i < numbers.length; i +=1){
@@ -31,17 +32,28 @@
 
 // ########## EXERCICIO 4 ###########
 
+// for (let i =0; i< numbers.length; i +=1){
+//     soma += numbers [i];
+// }
+
+// let resultado = soma/numbers.length;
+
+
+// if (resultado > 20){
+//     console.log (resultado + " Então o valor é maior que 20");
+// }
+// else {
+//     console.log ( resultado + " Então o valor é menor ou igual a 20");
+// }
+
+// ############### EXERCICIO 5 ##############
+
 for (let i =0; i< numbers.length; i +=1){
-    soma += numbers [i];
+      
+    if(numbers[i] > numeroMaior) {
+      numeroMaior = numbers [i];
+    }
+      
 }
 
-let resultado = soma/numbers.length;
-
-
-if (resultado > 20){
-    console.log (resultado + " Então o valor é maior que 20");
-}
-else {
-    console.log ( resultado + " Então o valor é menor ou igual a 20");
-}
-
+console.log (numeroMaior);
