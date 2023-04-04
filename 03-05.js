2aca20d20f01205f66ae28813674b05c53cf1890
João Cláudio <joaoclaudiobio@gmail.com>
2023-04-04 14:54:46 -0300

    Template Literal

---
+++ 03-05.js
@@ -0,0 +1,10 @@
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
+console.log ('Primeira linha \n' +
+              'Segunda Linha \n' +
+              'Terceira Lnha ');
+
+
+
+
+let meuNome = "João Cláudio";
+console.log (`Welcome ${meuNome}!`);
