64ae50e1c730ed742d824709b8e25136119e6eab
João Cláudio <joaoclaudiobio@gmail.com>
2023-04-04 15:10:05 -0300

    arrow function

--- 03-05.js
+++ 03-05.js
@@ -1,10 +1,26 @@
-console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
-console.log ('Primeira linha \n' +
-              'Segunda Linha \n' +
-              'Terceira Lnha ');
+
+// ###### Template Literals ########
+
+// console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
+// console.log ('Primeira linha \n' +
+//               'Segunda Linha \n' +
+//               'Terceira Lnha ');
 
 
 
 
-let meuNome = "João Cláudio";
-console.log (`Welcome ${meuNome}!`);
+// let meuNome = "João Cláudio";
+// console.log (`Welcome ${meuNome}!`);
+
+// ########## ARROWS FUCNTION ###########
+
+//  ----- Esta é uma função normal -----
+const soma = function (num1, num2) {
+    return num1 + num2;   
+}
+
+// ------- esta é uma arrow function ------
+
+const sub = (num1, num2) => num1 - num2;
+
+console.log (sub(5,3));
